ee59ad95eee0b75e887fb3d6570e2d330fed48bd
karaware <33566352+karaware@users.noreply.github.com>
2021-11-25 11:46:15 +0900

    Update recommendTweet.py

--- recommendTweet.py
+++ recommendTweet.py
@@ -4,10 +4,10 @@
 import tweepy
 
 def tweet(tweetDir):
-    CK = 'F9775TKQrUgwws9uJm7qr0bw9'
-    CS = 'UDPVHjwVMaOf3HMHJ2U68s2ssPxv5Kf9LAePW3O2x2n3baSV7V'
-    AT = '1384009908178198529-g8yEdNsWEhlsxCEsjqIhOaoAdFnLUP'
-    AS = 'RLT6juFbVbPcHHe3V9ERbjSvYmeZmc68Ppcrlb3OPUftr'
+    CK = 'XXXXX'
+    CS = 'XXXXX'
+    AT = 'XXXXX'
+    AS = 'XXXXX'
 
     auth = tweepy.OAuthHandler(CK, CS)
     auth.set_access_token(AT, AS)
